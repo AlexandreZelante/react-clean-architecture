96ae496f80ba16eea2f97c30346eda1b416b09b3
Alexandre Zelante <alexandre.zelante@outlook.com>
2022-09-28 22:14:36 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,5 +1,9 @@
 // Creating a specific interface for POST due to Interface Segregation Principle
 // This approach makes it easier to implement this interface because if I had a "get" method I should implement that as well
+
+export type HttpPostParams = {
+  url: string
+}
 export interface HttpPostClient {
-  post(url: string): Promise<void>
+  post(params: HttpPostParams): Promise<void>
 }
--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,10 +1,10 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client";
+import { HttpPostClient, HttpPostParams } from "data/protocols/http/http-post-client";
 
 export class HttpPostClientSpy implements HttpPostClient { // Spy é um "dublê", ele captura valores para comparar e também cria dados fakes
   url?: string
 
-  async post(url: string): Promise<void> {
-    this.url = url;
+  async post(params: HttpPostParams): Promise<void> {
+    this.url = params.url;
     return Promise.resolve()
   }
 }
